79941d847cc8e2e4d238d1250a33bcb492f207aa
bitly123 <56836463+bitly123@users.noreply.github.com>
2019-10-24 23:26:49 +0530

    Update 5.sh

--- 5calc/5.sh
+++ 5calc/5.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 clear
 sudo apt-get install openjdk-8-jdk openjdk-8-jre
 clear
-sudo apt-get install curl
+sudo apt-get install wget
 clear
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mBvXMPGnBED6EuOMAcrtyIZv7_9ECrNl' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mBvXMPGnBED6EuOMAcrtyIZv7_9ECrNl" -O jdk-8u60-linux-x64.tar.gz && rm -rf /tmp/cookies.txt
 clear
